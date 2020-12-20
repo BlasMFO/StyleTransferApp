2997f92303a28e5e243bdc0bba785318d0bf075a
BlasMFO <blasortellado@gmail.com>
2020-12-20 01:01:08 -0300

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # StyleTransferApp
-This app uses deep learning to compose one image usint the style of other image.
+
+<p>This app uses deep learning to compose one image using the style of another image.</p>
+<p>This app uses docker to create two microservices, the first is aim to handle a Tensorflow DNN model that makes the style transfer. And, the second microservice is a Flask application that handles the web request and displays the results</p>
+
+</br>
+
+<h2>Interface</h2>
+
+Some screenshots of the web app:
+
+* Open your browser in localhost on port 5000 and you will see this page:
+![screen_shot01](./screenshots/ScreenShot01.png)
+
+* Select a source and a style image to process. The program will mimic the style image into the source image.
+![screen_shot02](./screenshots/ScreenShot02.png)
+
+* See the results and download them if you wish:
+![screen_shot03](./screenshots/ScreenShot03.png)
+
+</br>
+
+<h3>Notes</h3>
+
+* The first time you compose up the services, it could take a few minutes since a DNN model is downloaded and a couple of python libraries are installed.
+* The chace for the processed image can be found in <i style="color:MediumSeaGreen;">./python/static/temp/</i>. In case you want to re-visid the processed images or erase the files.
---
+++ python/app.py
@@ -0,0 +1,101 @@
+import os
+import string
+import random
+import json
+import requests
+import numpy as np
+import tensorflow as tf
+import PIL.Image
+import functools
+from datetime import datetime
+
+from flask import Flask, request, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+contentPath, stylePath = '', ''
+modelUri = f"http://tf_serve:8501/v1/models/tftransfer:predict"
+outputDir = './static/temp'
+
+# region functions
+def generate_filename():
+	"""Generates a random file name with a timestamp."""
+	theString = datetime.now().strftime("%Y%m%d_%H%M%S_") + ''.join(random.choices(string.ascii_lowercase, k=5)) 
+	return theString + '.jpg'
+
+def tensor_to_image(tensor):
+	"""Convert the tensor response to a PIL image."""
+	tensor = tensor*255
+	tensor = np.array(tensor, dtype=np.uint8)
+	if np.ndim(tensor) > 3:
+		assert tensor.shape[0] == 1
+		tensor = tensor[0]
+	return PIL.Image.fromarray(tensor)
+
+def load_img(imgPath):
+	"""Load an image for the DNN model."""
+	max_dim = 512
+	img = tf.io.read_file(imgPath)
+	img = tf.image.decode_image(img, channels=3)
+	img = tf.image.convert_image_dtype(img, tf.float32)
+	
+	shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+	long_dim = max(shape)
+	scale = max_dim / long_dim
+	
+	new_shape = tf.cast(shape * scale, tf.int32)
+	
+	img = tf.image.resize(img, new_shape)
+	img = img[tf.newaxis, :]
+	return img
+
+def get_result(contentPath, stylePath, resultPath):
+	"""Get the result."""
+	contentImage = load_img(contentPath)
+	styleImage = load_img(stylePath)
+	contentImage = contentImage.numpy().tolist()
+	styleImage = styleImage.numpy().tolist()
+	data = json.dumps({
+		'inputs': {
+			'placeholder': contentImage,
+			'placeholder_1': styleImage
+		}
+	})
+	response = requests.post(modelUri, data=data.encode())
+	response = response.json()
+	data = np.array(response['outputs'], dtype='float32')
+	img = tensor_to_image(data)
+	img.save(resultPath)
+# endregion functions
+
+# region routes
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		uploaded_file1 = request.files['file1']
+		uploaded_file2 = request.files['file2']
+		if (uploaded_file1.filename != '') and (uploaded_file2.filename != '') and (uploaded_file1.filename[-3:] in ['jpg', 'png', 'jpeg']) and (uploaded_file2.filename[-3:] in ['jpg', 'png', 'jpeg']):
+			contentPath = os.path.join(outputDir, generate_filename())
+			stylePath = os.path.join(outputDir, generate_filename())
+			resultPath = os.path.join(outputDir, generate_filename())
+			uploaded_file1.save(contentPath)
+			uploaded_file2.save(stylePath)
+			get_result(contentPath, stylePath, resultPath)
+			result = {
+				'contentPath': contentPath,
+				'stylePath': stylePath,
+				'resultPath': resultPath
+			}
+			return render_template('show.html', result=result)
+	return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+# endregion routes
+
+# region main
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
+# endregion main
